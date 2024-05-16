7c37e7854520fafa9d69efd1e676082a99a1dda1
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-05-16 13:50:05 +0000

    division challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,6 +139,7 @@ function displayDivisionQuestion(operand1,operand2) {
     
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2
     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1
+    document.getElementById("operand1").textContent = operand1*operand2
     document.getElementById('operator').textContent = "/";
 
 }
